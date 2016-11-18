8c17280a4ea98fbef4f4cc8ddd28cb54fc9c536a
Frank Kung <kfanjian@gmail.com>
2016-11-18 22:31:32 +0800

    Rename.

--- ua.go
+++
@@ -1,6 +0,0 @@
-package jwp
-
-const (
-	Chrome54         = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36"
-	AppleiPhoneiOS91 = `Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1`
-)
