d4a80c87e1ef6d36ead668940c6b3a050cbe8220
Frank Kung <kfanjian@gmail.com>
2016-11-19 06:36:24 +0800

    Test Delete method.

--- zzzzz_test.go
+++ zzzzz_test.go
@@ -1,6 +1,21 @@
 package jwp
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDelete(t *testing.T) {
+	err := client.Delete()
+	require.NoError(t, err)
+
+	list, err2 := client.ListSessions()
+	require.NoError(t, err2)
+	assert.Equal(t, []SessionItem{}, list.Value)
+}
 
 func TestCleanup(t *testing.T) {
 	t.Log("clear resources")
