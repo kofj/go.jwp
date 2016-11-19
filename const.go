54b0fd0cb5ebecc83d881be822cee575a6d5dacc
Frank Kung <kfanjian@gmail.com>
2016-11-19 10:03:05 +0800

    Add status code consts.

--- const.go
+++ const.go
@@ -9,3 +9,32 @@ const (
 const (
 	BlankScreen = "iVBORw0KGgoAAAANSUhEUgAAAZAAAAEsCAYAAADtt+XCAAAACXBIWXMAAAsTAAALEwEAmpwYAAAB6ElEQVR4nO3BMQEAAADCoPVPbQ0PoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHg3VJUAAfkd0+cAAAAASUVORK5CYII="
 )
+
+// status code
+const (
+	StatusSuccess                    = 0
+	StatusNoSuchDriver               = 6
+	StatusNoSuchElement              = 7
+	StatusNoSuchFrame                = 8
+	StatusUnknownCommand             = 9
+	StatusStaleElementReference      = 10
+	StatusElementNotVisible          = 11
+	StatusInvalidElementState        = 12
+	StatusUnknownError               = 13
+	StatusElementIsNotSelectable     = 15
+	StatusJavaScriptError            = 17
+	StatusXPathLookupError           = 19
+	StatusTimeout                    = 21
+	StatusNoSuchWindow               = 23
+	StatusInvalidCookieDomain        = 24
+	StatusUnableToSetCookie          = 25
+	StatusUnexpectedAlertOpen        = 26
+	StatusNoAlertOpenError           = 27
+	StatusScriptTimeout              = 28
+	StatusInvalidElementCoordinates  = 29
+	StatueIMENotAvailable            = 30
+	StatusIMEEngineActivationFailed  = 31
+	StatusInvalidSelector            = 32
+	StatusSessionNotCreatedException = 33
+	StatusMoveTargetOutOfBounds      = 34
+)
