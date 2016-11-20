682d2160fb1f53f969736ac90e0ccdf97e4d1127
Frank Kung <kfanjian@gmail.com>
2016-11-20 10:58:59 +0800

    Add function CheckStatus: convert status to error.

--- errors.go
+++ errors.go
@@ -13,3 +13,79 @@ func CheckHTTPStatus(res *http.Response, body []byte) (err error) {
 	}
 	return
 }
+
+func CheckStatus(status int) (err error) {
+	switch status {
+	case StatusSuccess:
+		err = nil
+	case StatusNoSuchDriver:
+		err = errors.New("StatusNoSuchDriver")
+
+	case StatusNoSuchElement:
+		err = errors.New("StatusNoSuchElement")
+
+	case StatusNoSuchFrame:
+		err = errors.New("StatusNoSuchFrame")
+
+	case StatusUnknownCommand:
+		err = errors.New("StatusUnknownCommand")
+
+	case StatusStaleElementReference:
+		err = errors.New("StatusStaleElementReference")
+
+	case StatusElementNotVisible:
+		err = errors.New("StatusElementNotVisible")
+
+	case StatusInvalidElementState:
+		err = errors.New("StatusInvalidElementState")
+
+	case StatusUnknownError:
+		err = errors.New("StatusUnknownError")
+
+	case StatusElementIsNotSelectable:
+		err = errors.New("StatusElementIsNotSelectable")
+
+	case StatusJavaScriptError:
+		err = errors.New("StatusJavaScriptError")
+
+	case StatusXPathLookupError:
+		err = errors.New("StatusXPathLookupError")
+
+	case StatusTimeout:
+		err = errors.New("StatusTimeout")
+
+	case StatusNoSuchWindow:
+		err = errors.New("StatusNoSuchWindow")
+
+	case StatusInvalidCookieDomain:
+		err = errors.New("StatusInvalidCookieDomain")
+
+	case StatusUnableToSetCookie:
+		err = errors.New("StatusUnableToSetCookie")
+
+	case StatusUnexpectedAlertOpen:
+		err = errors.New("StatusUnexpectedAlertOpen")
+
+	case StatusNoAlertOpenError:
+		err = errors.New("StatusNoAlertOpenError")
+
+	case StatusScriptTimeout:
+		err = errors.New("StatusScriptTimeout")
+
+	case StatusInvalidElementCoordinates:
+		err = errors.New("StatusInvalidElementCoordinates")
+
+	case StatusIMEEngineActivationFailed:
+		err = errors.New("StatusIMEEngineActivationFailed")
+
+	case StatusInvalidSelector:
+		err = errors.New("StatusInvalidSelector")
+
+	case StatusSessionNotCreatedException:
+		err = errors.New("StatusSessionNotCreatedException")
+
+	case StatusMoveTargetOutOfBounds:
+		err = errors.New("StatusMoveTargetOutOfBounds")
+	}
+	return
+}
