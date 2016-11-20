6ec9f723f504b0eed4536ca2a7aaac80dfcd1d30
Frank Kung <kfanjian@gmail.com>
2016-11-20 14:00:03 +0800

    Add method to set current window size.

--- session_window.go
+++ session_window.go
@@ -1,6 +1,7 @@
 package jwp
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -40,6 +41,55 @@ func (c *Client) GetCurrentWindowSize() (size *WindowSize, err error) {
 	}
 
 	size = &resp.Value
+
+	return
+}
+
+func (c *Client) SetCurrentWindowSize(size *WindowSize) (err error) {
+	var (
+		req  *http.Request
+		res  *http.Response
+		body []byte
+		buf  = new(bytes.Buffer)
+		resp ApiWindowSize
+	)
+
+	if size.Height < 100 {
+		size.Height = 100
+	}
+	if size.Width < 100 {
+		size.Width = 100
+	}
+
+	err = json.NewEncoder(buf).Encode(*size)
+	if err != nil {
+		return
+	}
+
+	req, err = http.NewRequest("POST", c.Server+"/session/"+c.SessionID+"/window/current/size", buf)
+	if err != nil {
+		return
+	}
+
+	res, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	defer res.Body.Close()
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	if err = CheckHTTPStatus(res, body); err != nil {
+		return
+	}
+
+	json.Unmarshal(body, &resp)
+	if err = CheckStatus(resp.Status); err != nil {
+		return
+	}
 
 	return
 }
