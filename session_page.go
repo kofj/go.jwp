554692dbe67750cd33e683b5905f8d05aeea0683
Frank Kung <kfanjian@gmail.com>
2016-11-21 00:31:49 +0800

    Add method to execute script.

--- session_page.go
+++ session_page.go
@@ -98,3 +98,56 @@ func (c *Client) Back() (err error) { return }
 // POST /session/:sessionId/refresh
 // Refresh the current page.
 func (c *Client) Refresh() (err error) { return }
+
+// POST /session/:sessionId/execute
+/* Inject a snippet of JavaScript into the page for execution
+ * in the context of the currently selected frame. The executed script
+ * is assumed to be synchronous and the result of evaluating the script
+ * is returned to the client.
+ *
+ * JSON Parameters:
+ * script - {string} The script to execute.
+ * args - {Array.<*>} The script arguments.
+ */
+func (c *Client) Execute(es *ExecuteScript) (result string, err error) {
+	var (
+		req  *http.Request
+		res  *http.Response
+		body []byte
+		buf  = new(bytes.Buffer)
+		resp ApiExecuteScriptResult
+	)
+
+	err = json.NewEncoder(buf).Encode(*es)
+	if err != nil {
+		return
+	}
+
+	req, err = http.NewRequest("POST", c.Server+"/session/"+c.SessionID+"/execute", buf)
+	if err != nil {
+		return
+	}
+
+	res, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	defer res.Body.Close()
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	if err = CheckHTTPStatus(res, body); err != nil {
+		return
+	}
+
+	json.Unmarshal(body, &resp)
+	if err = CheckStatus(resp.Status); err != nil {
+		return
+	}
+	result = resp.Value
+
+	return
+}
