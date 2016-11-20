afe18b77ae9724c635f8e6d3fb03f99c4600196a
Frank Kung <kfanjian@gmail.com>
2016-11-21 00:32:12 +0800

    Test Execute method.

--- session_page_test.go
+++ session_page_test.go
@@ -20,3 +20,12 @@ func TestOpen(t *testing.T) {
 	url, _ := client.GetCurrentPageURL()
 	require.Equal(t, "https://www.baidu.com/", url)
 }
+
+func TestExecute(t *testing.T) {
+	ret, err := client.Execute(&ExecuteScript{
+		Script: "return document.getElementsByTagName('title')[0].text",
+		Args:   []map[string]interface{}{{"prefix": "title"}},
+	})
+	require.NoError(t, err)
+	assert.Equal(t, "百度一下，你就知道", ret)
+}
