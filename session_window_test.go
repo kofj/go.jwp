2e728d5ad35655d4a15a511b1a263f9d8d0a124b
Frank Kung <kfanjian@gmail.com>
2016-11-20 14:00:21 +0800

    Test SetCurrentWindowSize.

--- session_window_test.go
+++ session_window_test.go
@@ -14,3 +14,13 @@ func TestGetCurrentWindowSize(t *testing.T) {
 	assert.Equal(t, 300, size.Height)
 	assert.Equal(t, 400, size.Width)
 }
+
+func TestSetCurrentWindowSize(t *testing.T) {
+	size := &WindowSize{Height: 800, Width: 600}
+	err := client.SetCurrentWindowSize(size)
+	require.NoError(t, err)
+	size, err = client.GetCurrentWindowSize()
+	require.NoError(t, err)
+	assert.Equal(t, 800, size.Height)
+	assert.Equal(t, 600, size.Width)
+}
